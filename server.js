457602b2eedc2be0f6de0f5bdc3e21dee186c821
HarishKondakindi <harishkondakindi97@gmail.com>
2016-10-28 21:10:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/page.css',function(req,res) {
+    res.sendFile(path.join(__dirname,'ui','page.css'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
